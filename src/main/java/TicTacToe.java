6f174daa8bd2e66f95514c8dd125b5ac7caa11f1
MohitMandal1099 <https://github.com/MohitMandal1099/OOPSBannerApp.git>
2026-04-29 10:09:59 +0530

    new uc

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -13,7 +13,7 @@ public class TicTacToe {
     }
 
     public static void displayBoard() {
-        System.out.println("--------------");
+        System.out.println("-------------");
         for (int i = 0; i < 3; i++) {
             System.out.print("| ");
             for (int j = 0; j < 3; j++) {
